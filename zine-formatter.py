db8e0b52149452e5bc95feaafa3665239c0764bf
Julia Evans <julia@jvns.ca>
2019-02-27 21:57:35 -0500

    add latex version

--- zine-formatter.py
+++ zine-formatter.py
@@ -10,6 +10,7 @@ def parse_args():
     parser.add_argument('--print-output', required=True)
     parser.add_argument('--view-output', required=True)
     parser.add_argument('--size')
+    parser.add_argument('--trim-bottom')
     parser.add_argument('--cover')
     parser.add_argument('--bw', dest='bw', action='store_true')
     parser.add_argument('--rotate', dest='rotate', action='store_true')
@@ -24,17 +25,23 @@ def multiple_of_four(value):
     return i
 
 def run(args):
-    print(args)
+    if len(args) > 10:
+        print(args)
+    else:
+        print(' '.join(args))
     if type(args) == list:
         subprocess.check_call(args)
     else:
         subprocess.check_call(args.split())
 
 def temp_filename():
-    return tempfile.mktemp('.pdf')
+    return tempfile.NamedTemporaryFile(delete=False, dir='.', suffix='.pdf').name
 
 
-def make_zine(zine, pages, print_output, view_output, cover=None, size=None, bw=False, rotate=False):
+def make_zine(zine, pages, print_output, view_output, trim_bottom='0mm', cover=None, size=None, bw=False, rotate=False):
+    cropped = temp_filename()
+    run(['pdfcrop', zine, cropped])
+    zine = cropped
     if rotate:
         zine_rotated = temp_filename()
         cover_rotated = temp_filename()
@@ -47,22 +54,23 @@ def make_zine(zine, pages, print_output, view_output, cover=None, size=None, bw=
             run("pdftk {cover} rotate 1-1west output {cover_rotated}".format(cover=cover,
                 cover_rotated=cover_rotated))
             cover = cover_rotated
-    if bw:
-        zine_bw = temp_filename()
-        run("convert -density 600 {zine} -negate -threshold 0 -negate {zine_bw}".format(zine=zine,
-            zine_bw=zine_bw))
-        zine = zine_bw
-
     if cover is not None:
-        inside = temp_filename()
         cover_resized = temp_filename()
+        zine_resized = temp_filename()
         zine_step1 = temp_filename()
+        inside = temp_filename()
         run("pdftk {zine} cat 2-{pages} output {inside}".format(
             zine=zine,
             pages=pages,
             inside=inside))
+        run(['pdfjam', '--outfile', zine_resized, '--papersize', '{4in,6in}', inside])
         run(['pdfjam', '--outfile', cover_resized, '--papersize', '{4in,6in}', cover])
-        run(['pdftk', cover_resized, inside, 'cat', 'output', zine_step1])
+        if bw:
+            zine_bw = temp_filename()
+            run("convert -density 600 {zine_resized} -negate -threshold 0 -negate {zine_bw}".format(zine_resized=zine_resized,
+                zine_bw=zine_bw))
+            zine_resized = zine_bw
+        run(['pdftk', cover_resized, zine_resized, 'cat', 'output', zine_step1])
     else:
         inside = temp_filename()
         run("pdftk {zine} cat 1-{pages} output {inside}".format(zine=zine, pages=pages, inside=inside))
@@ -73,11 +81,11 @@ def make_zine(zine, pages, print_output, view_output, cover=None, size=None, bw=
     elif size == 'a4':
         size_arg = '--a4paper'
     run(['pdfjam', '--booklet', 'true', '--landscape', '--suffix', 'book', size_arg,
-    '--signature', '12', '--booklet', 'true', '--scale', '1.0', '--offset', '0cm 0cm',
+    '--signature', '12', '--booklet', 'true', '--trim', '0mm 0mm %s 0mm' % trim_bottom, '--clip' , 'true', '--scale', '1.0', '--offset', '0cm 0cm',
     '--landscape', view_output, '-o', print_output])
 
 if __name__ == '__main__':
     args = parse_args()
-    make_zine(args.zine, args.pages, args.print_output, args.view_output, size=args.size, cover=args.cover, bw=args.bw, rotate=args.rotate)
+    make_zine(args.zine, args.pages, args.print_output, args.view_output, trim_bottom=args.trim_bottom, size=args.size, cover=args.cover, bw=args.bw, rotate=args.rotate)
 
 
---
+++ zine-maker-tex.py
@@ -0,0 +1,72 @@
+import subprocess
+import tempfile
+import sys
+import argparse
+from jinja2 import Template
+
+TEMPLATE = Template('''
+\\batchmode
+\\documentclass[{{papersize}},landscape]{article}
+\\usepackage[utf8]{inputenc}
+\\usepackage[monochrome]{xcolor}
+\\usepackage{pdfpages}
+
+\\begin{document}
+
+{% if should_print -%}
+\\includepdfmerge[booklet=true,landscape=true,signature=12,scale=1.0,offset=0cm 0cm,trim=0mm {{trim_bottom}} 00mm 00mm,clip]{
+{%- else %}
+\includepdfmerge[trim=0mm {{trim_bottom}} 0mm 0mm,clip]{
+{%- endif %}
+{{cover}},
+{{zine}},2-{{pages}}}
+
+\end{document}
+''')
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--zine', required=True)
+    parser.add_argument('--pages', required=True)
+    parser.add_argument('--print-output', required=True)
+    parser.add_argument('--view-output')
+    parser.add_argument('--papersize', default='letter')
+    parser.add_argument('--trim-bottom', default='0mm')
+    parser.add_argument('--cover')
+    parser.add_argument('--bw', dest='bw', action='store_true')
+    parser.add_argument('--rotate', dest='rotate', action='store_true')
+
+    return parser.parse_args()
+
+def run(args):
+    print(' '.join(args))
+    subprocess.check_call(args)
+
+
+def render(zine=None, pages=None, should_print=None, trim_bottom=None, cover=None, rotate=None,
+        papersize=None, filename=None):
+    latex = TEMPLATE.render(zine=zine, pages=pages,
+            should_print=should_print, trim_bottom=trim_bottom,
+            cover=cover,
+            rotate=rotate, papersize=papersize)
+    tex_file = '%s.tex' % filename.replace('.pdf', '')
+    with open(tex_file, 'w') as f:
+        f.write(latex)
+    run(["pdflatex", tex_file])
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if args.trim_bottom:
+        run(["pdfcrop", "--margins", "0 0 00 800", args.cover, "asdf2.pdf"])
+        run(["pdfjam", "--outfile", "asdf.pdf", "--papersize", "{8.5in,11in}", "asdf2.pdf"])
+    if args.view_output:
+        render(zine=args.zine, pages=args.pages,
+                should_print=False, trim_bottom=args.trim_bottom,
+                cover='asdf.pdf',
+                rotate=args.rotate, papersize=args.papersize, filename=args.view_output)
+    render(zine=args.zine, pages=args.pages,
+            should_print=True, trim_bottom=args.trim_bottom,
+            cover='asdf.pdf',
+            rotate=args.rotate, papersize=args.papersize, filename=args.print_output)
+
